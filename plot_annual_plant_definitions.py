70d5bba1ea7c9fe57fee1e85452c58407c03ca4a
J.W.SPAAK <jurgspaak@unamur.be>
2018-11-14 10:39:08 +0100

    Update figures
    Changes of figures conform to 5. complete verison of manuscript

--- plot_annual_plant_definitions.py
+++ plot_annual_plant_definitions.py
@@ -17,6 +17,8 @@ lamb1 = 1.5
 lamb2 = 3
 
 A12 = np.linspace(-A22/(lamb2-1)+1e-3,2.5, rep)
+sign = -1
+interspec = sign*A12*A21/A11/A22 # interspecific interaction strength
 
 ND = {}
 FD = {}
@@ -100,13 +102,13 @@ for l in range(rep):
         c[l] = brentq(NO_equate,0,1e3, args = (denom[:,l]))
     except ValueError:
         c[l] = brentq(NO_equate,0,1e10, args = (denom[:,l]))
-key = "Spaak & deLaender"   
+key = "Spaak & DeLaender"   
 ND[key] = 1-NO_spaak(c, denom)[0]
 FD[key] = np.log(lamb1/(1+c*A11/A22*(lamb2-1)))/np.log(lamb1)
 
 keys = ["Chesson","Carroll et al.", "Zhao et al.",
         "Godoy et al.", "Adler et al.",   "Bimler et al.","Saavedra et al.",
-        "Spaak & deLaender"]
+        "Spaak & DeLaender"]
 
 colors =  {keys[i]: rainbow(np.linspace(0, 1, len(keys)))[i]
                 for i in range(len(keys))}
@@ -116,113 +118,80 @@ fig = plt.figure(figsize = (9,9))
 
 ND_range = [-0.5,1.5]
 
-rect_facilitation = patches.Rectangle([A12[0],1],-A12[0],ND_range[1]-1,
-                                      fill = False, linestyle = "-.")
-rect_norm = patches.Rectangle([0,0],A11*A22/A21[0],1, fill = False)
-rect_comp = patches.Rectangle([A11*A22/A21[0],0],A12[-1], ND_range[0]
+rect_facilitation = patches.Rectangle([interspec[0],1],
+            -interspec[0], ND_range[1]-1, fill = False, linestyle = ":")
+rect_norm = patches.Rectangle([0,0],sign,1, fill = False)
+rect_comp = patches.Rectangle([sign*1,0],interspec[-1], ND_range[0]
                               , fill = False, linestyle = "--")
-
-rect_axes = patches.Rectangle([0,0],-A12[-1], ND_range[0]
-                              , fill = False, linestyle = ":")
 ax = plt.gca()
 ax.add_patch(rect_norm)
 ax.add_patch(rect_facilitation)
 ax.add_patch(rect_comp)
-
-# draw rest of axis line
-ax.add_patch(rect_axes)
           
 # plot NFD parameters          
 for key in keys:
-    plt.plot(A12, ND[key], label = key, linewidth = 2, alpha = 1, 
+    plt.plot(interspec, ND[key], label = key, linewidth = 2, alpha = 1, 
              color = colors[key])
 
 # add black dots
-plt.plot(0,1, 'o', color = "black")
-plt.plot(A11*A22/A21[0],0, '^', color = "black")
+plt.plot(0,1, 'o', color = "black", markersize = 10)
+plt.plot(sign*1,0, '^', color = "black", markersize = 10)
 
 
 # layout
-plt.legend()
+plt.legend(loc = "upper left")
 
 # axis limits
-plt.xlim(A12[[0,-1]])
-plt.xticks([0])
+plt.xlim(min(interspec), max(interspec))
+plt.xticks([0,sign*1])
 plt.ylim(*ND_range)
 plt.yticks([0,1])
 
 # labeling of interaction
 fs = 16
 offset_y = 0.15
-plt.text(A12[0]/2,ND_range[0]+offset_y,"positive", 
+plt.text(interspec[0]/2,ND_range[0]+offset_y,"positive", 
          ha = "center", fontsize = fs, backgroundcolor = "white")
-plt.text(A11*A22/A21[0]/2,ND_range[0]+offset_y,
+plt.text(sign*1/2,ND_range[0]+offset_y,
          "negative,\nweaker than\nintraspecific", 
          ha = "center",va = "center", fontsize = fs)
-plt.text((A11*A22/A21[0]+A12[-1])/2,ND_range[0]+offset_y,
+plt.text((sign*1+interspec[-1])/2,ND_range[0]+offset_y,
           "negative,\nstronger than\nintraspecific", backgroundcolor = "white",
          ha = "center",va = "center", fontsize = fs)
 
 
 
 # axis labels
-plt.xlabel(r'Interspecific competition ($\alpha_{12}$)', fontsize = 16)
+plt.xlabel(r'Interspecific competition ($\alpha$)', fontsize = 16)
 plt.ylabel(r'Niche difference $(\mathcal{N})$', fontsize = 16)
 
 fig.savefig("ND in annual plants.pdf")
 
-"""
 ###############################################################################
 # plotting the results for ND
-
-fig, ax = plt.subplots(ncols = 2, figsize = (12,9), sharex = True)
+fig = plt.figure(figsize = (9,9))
 
 ND_range = [-0.5,1.5]
-
-rect_facilitation = patches.Rectangle([A12[0],1],-A12[0],ND_range[1]-1,
-                                      alpha = 0.5, color = "green")
-rect_norm = patches.Rectangle([0,0],A11*A22/A21[0],1, alpha = 0.5)
-rect_comp = patches.Rectangle([A11*A22/A21[0],0],A12[-1], ND_range[0]
-                              , alpha = 0.5, color = "red")
-ax[0].add_patch(rect_norm)
-ax[0].add_patch(rect_facilitation)
-ax[0].add_patch(rect_comp)
           
 # plot NFD parameters          
 for key in keys:
-    ax[0].plot(A12, ND[key], label = key, linewidth = 2, alpha = 1, 
+    plt.plot(interspec, FD[key], label = key, linewidth = 2, alpha = 1, 
              color = colors[key])
-    ax[1].plot(A12, FD[key], label = key, linewidth = 2, alpha = 1, 
-             color = colors[key])
-
-# add black dots
-ax[0].plot(0,1, 'o', color = "black")
-ax[0].plot(A11*A22/A21[0],0, '^', color = "black")
-
 
 # layout
-ax[1].legend(bbox_to_anchor=(1, 0.85))
+plt.legend(loc = "upper left")
 
 # axis limits
-ax[0].set_xlim(A12[[0,-1]])
-ax[0].set_xticks([0])
-ax[0].set_ylim(*ND_range)
-ax[1].set_ylim(-1,2)
-ax[1].set_yticks([0,1])
+plt.xlim(min(interspec), max(interspec))
+plt.xticks([0,sign*1])
+plt.ylim(-3,3)
 
-# draw axis lines
-ax[0].axhline(y=0, color='black', linestyle=':')
-ax[0].axvline(x=0, color='black', linestyle=':')
-ax[1].axhline(y=0, color='black', linestyle=':')
-ax[1].axvline(x=0, color='black', linestyle=':')
+
+plt.axhline(y=0, color = "black", linestyle = ":")
+plt.axvline(x=0, color = "black", linestyle = ":")
 
 # axis labels
-ax[0].set_xlabel(r'Interspecific competition ($\alpha_{12}$)', fontsize = 16)
-ax[1].set_xlabel(r'Interspecific competition ($\alpha_{12}$)', fontsize = 16)
-ax[0].set_ylabel(r'Niche difference $(\mathcal{N})$', fontsize = 16)
-ax[1].set_ylabel(r'Fitness difference $(\mathcal{F})$', fontsize = 16)
-ax[0].set_title("A")
-ax[1].set_title("B")
+plt.xlabel(r'Interspecific competition ($\alpha$)', fontsize = 16)
+plt.ylabel(r'Fitness difference $(\mathcal{F})$', fontsize = 16)
 
-
-fig.savefig("NFD in annual plants.pdf")"""
+fig.savefig("FD in annual plants.pdf")
--- plots.py
+++ plots.py
@@ -8,118 +8,79 @@ import numpy as np
 
 ###############################################################################
 # Example of Mac Arthur resource model with NO
-"""
-r = np.linspace(0,10,11)
-peaks = np.array([2.3,3.7,6.7]) # peaks of the utilization functions
-g = np.exp(-(r-peaks[:,np.newaxis])**2)
-
-fig, ax = plt.subplots(1,2, figsize = (8,3), sharex = False, sharey = True)
-ax[0].plot(r,g[0], "red", r,g[1], "blue", linewidth = 2)
-ax[0].plot(r,1.3*g[0], "r:")
-#ax[0].plot(r, 0.5*g[0], ":r")
-ax[0].fill(r,np.amin(g[:2],axis = 0), color = "purple",alpha = 0.5)
-ax[0].axis([0,6,0,1.5])
-ax[0].text(3,0.1,"Niche\nOverlap", ha = "center")
-
-ax[1].plot(r, g[0], "red", r,g[1], "blue", r,g[2], "green", linewidth = 2)
-
-ax[1].fill(r,np.amin(g[:2],axis = 0), color = "purple",alpha = 0.5)
-ax[1].fill(r,np.amin(g[1:],axis = 0), color = "cyan",alpha = 0.5)
-
-ax[0].set_xticks([])
-ax[1].set_xticks([])
-ax[0].set_yticks([])
-ax[0].set_xlabel("Resource")
-ax[1].set_xlabel("Resource")
-ax[0].set_ylabel("Utilization")
-ax[0].set_title("A")
-ax[1].set_title("B")
-
-
-fig.savefig("Figure, Utilization functions.pdf")
-"""
-
-fig ,ax = plt.subplots(1,2,figsize = (11,4), sharey = True)
+fig = plt.figure()
 bars = np.array([0.1,0.2,0.3,0.4,0.5,0.4,0.3,0.2,0.1])
 x = np.arange(len(bars))
 
 bar_edges = np.array([[0,0,0.8,0.8],[0,1,1,0]])
 
 # draw lines of to high total resource use
-for i in range(len(bars)):
-    ax[0].plot(x[i]+bar_edges[0], bars[i]+0.5*bars[i]*bar_edges[1], "r--")
+#for i in range(len(bars)):
+#    plt.plot(x[i]+bar_edges[0], bars[i]+0.5*bars[i]*bar_edges[1], "r--")
 
 # barplots of dependence on limiting factor
-ax[0].bar(x,bars, color = "red", alpha = 0.5)
-ax[0].bar(x+4,bars, color = "blue", alpha = 0.5)
-
-# barplots on multispecies
-ax[1].bar(x,bars, color = "red", alpha = 0.5)
-ax[1].bar(x+4,bars, color = "blue", alpha = 0.5)
-ax[1].bar(x+10,bars, color = "green", alpha = 0.5)
+plt.bar(x,bars, color = "red", alpha = 0.5)
+plt.bar(x+4,bars, color = "blue", alpha = 0.5)
 
 fs = 14
 #ax[0].text(6.4,0.04,"Niche\nOverlap", ha = "center",fontsize = fs-2,
 #        bbox = {"facecolor": "blueviolet","boxstyle": "round"})
 
-ax[0].set_xticks([])
-ax[1].set_xticks([])
-ax[0].set_yticks([])
+plt.xticks([])
+plt.yticks([])
 
-ax[0].set_xlabel("Limiting factor", fontsize = fs)
-ax[1].set_xlabel("Limiting factor", fontsize = fs)
-ax[0].set_ylabel("Dependence on limiting factor", fontsize = fs)
-ax[1].set_ylabel("Dependence on limiting factor", fontsize = fs)
-ax[0].set_title("A")
-ax[1].set_title("B")
+plt.xlabel("Limiting factor", fontsize = fs)
+plt.ylabel("Dependence on limiting factor", fontsize = fs)
 
-ax[0].axis([-0.3,13.1,None,None])
-ax[1].axis([-0.3,19.1,None,None])
+plt.axis([-0.3,13.1,None,0.6])
 
 fig.savefig("Figure, Limiting factors.pdf")
-###############################################################################
-# Linear interpolation plot
-
-fig = plt.figure(figsize = (7,7))
-plt.axes(frame_on = False)
-offset = 0.2
-plt.plot([0,1],[0,1],'r-o')
-plt.axis([-offset,1+offset, -offset, 1+offset])
-
-ri = 0.7
-plt.plot([ri,ri,-offset],[-offset,ri,ri], '--')
-
-fs = 16
-plt.xticks([0,ri,1], [r"$f_i(c_jN_j^*,0)$",r"$f_i(0,N_j^*)$",r"$f_i(0,0)$"],
-           fontsize = fs)
-plt.yticks([0,ri,1], [0,r"$\mathcal{N}_i$",1],fontsize = fs)
-plt.tick_params(length = 20,top = False, right = False)
-arrow = 0.035
-plt.arrow(-offset+arrow,-offset+arrow, 1+offset+arrow,0)
-plt.arrow(-offset+arrow,-offset+arrow, 0, 1+offset+arrow)
-
-fig.savefig("Linear interpolation.pdf")
 
 ###############################################################################
-# Coexistence region
-
+# Extended regions
 fig = plt.figure(figsize = (7,7))
 
-x = np.linspace(0,1-1e-2,101)
+x = np.linspace(-1,3,101)
 plt.plot(x,x/(1-x), "black")
-plt.axis([0,1,-1,5])
+plt.axis([x[0],2,-1,4])
 plt.ylabel(r"$-\mathcal{F}$", fontsize = fs)
 plt.xlabel(r"$\mathcal{N}$", fontsize = fs)
 
-no_coex_x = np.append(x,0)
-no_coex_y = np.append(x/(1-x),5)
-plt.fill(no_coex_x,no_coex_y, color = "red",alpha = 0.5)
+plt.axhline(y=0, color = "black", linestyle = ":")
+plt.axhline(y=-1, color = "black", linestyle = ":")
+plt.axvline(x=0, color = "black", linestyle = ":")
+plt.axvline(x=1, color = "black", linestyle = ":")
 
-coex_x = np.append(x,[1,1,0])
-coex_y = np.append(x/(1-x),[6,-1,-1])
-plt.fill(coex_x,coex_y, color = "green",alpha = 0.5)
+plt.xticks([0,1])
+plt.yticks([0,-1])
 
-plt.text(0.5,-0.2,"Coexistence", ha = "center", fontsize = fs)
-plt.text(0.5,2,"Competitive\nExclusion", ha = "center", fontsize = fs)
+# plot the varius definitions
+plt.plot([-0.5,-0.5], [-0.1,0.1], 'o', 
+         color = "black", label = "priority effects")
 
-fig.savefig("Coexistence region.pdf")
+plt.plot([0,0], [0,0], '^', 
+         color = "black", label = "neutrality")
+
+plt.plot([0.2,0.2], [-0.3,3], 'v', color = "black", label = "comp. exclusion")
+
+plt.plot([0.6,0.6], [-0.5,1.2], '*', color = "black", label = "stable coex.")
+
+plt.plot([1.2,0.8], [2,-0.8], '+', markersize = 15,
+         color = "black", label = "parasitism")
+
+plt.plot([1.4,1.4], [-0.9,1.7], 's', 
+         color = "black", label = "mutualism")
+
+
+
+
+
+coex_x = np.append(np.linspace(x[0],1-1e-3),x[[-1,-1,0]])
+coex_y = coex_x/(1-coex_x)
+coex_y[-1] = -1
+coex_y[np.isinf(coex_y)] = 10**10
+plt.fill(coex_x,coex_y, color = "grey",alpha = 0.5)
+
+plt.legend(numpoints = 1)
+
+fig.savefig("Extended Coexistence region.pdf")
