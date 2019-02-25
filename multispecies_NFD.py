ccd32136cae43c1aeae3ec59821be8cfaceae15f
jurg.spaak@unamur.be <jurg.spaak@unamur.be>
2019-02-25 13:31:49 +0100

    Add comparison to simple NFD from uniform matrix

--- multispecies_NFD.py
+++ multispecies_NFD.py
@@ -3,20 +3,8 @@ import matplotlib.pyplot as plt
 
 from nfd_definitions.numerical_NFD import NFD_model
 
-def find_real_communities(A_prime,r_prime):
-    
-    # compute equilibrium densities of entire community
-    equi_prime = np.linalg.solve(A_prime,r_prime)
-    # corresponds to diagonal matrix of species densities
-    diag_equi_prime = equi_prime.reshape(len(A_prime),1,n)
-    
-    # find feasible (N^*>0) and stable (negative eigenvalues of Jacobi) com.
-    feasible = np.all(equi_prime>0,axis = 1)
-    stable = np.all(np.real(np.linalg.eigvals(-diag_equi_prime*A_prime))<0,
-                    axis = -1)
-    
+def find_real_communities(A,r_prime):
     # retain only feasible and stable communities
-    A = A_prime[feasible & stable]
     n_com = len(A)
     
     # compute equilibrium densities where one species is absent
@@ -39,10 +27,9 @@ def find_real_communities(A_prime,r_prime):
     sub_feasible = np.sum(sub_equi>0, axis = (1,2)) == n*(n-1)
     sub_stable = np.all(np.real(sub_jacobi)<0, axis = (1,2))
     
-    real = feasible & stable
+    real = np.full(n_com, True, dtype = bool)
     real[real] = sub_feasible & sub_stable
-    return (real, A_prime[real], equi_prime[real],
-                    sub_equi[sub_feasible & sub_stable])
+    return (real, A_prime[real], sub_equi[sub_feasible & sub_stable])
     
 def NFD_LV_multispecies(A,sub_equi, r = 1):
     # compute the two species niche overlap
@@ -51,7 +38,7 @@ def NFD_LV_multispecies(A,sub_equi, r = 1):
                 np.einsum("nij,nji,nii,njj->nij",A,A,1/A,1/A)))
     
     # NO is a weighted average of the two species NO
-    # weights = sqrt(a_ij/a_ji*a_jj)
+    # weights = sqrt(a_ij/a_ji*a_jj), remove a_ii by division
     weights = np.sqrt(np.abs(np.einsum("nij,nji,njj->nij",A,1/A,A)))*sub_equi
     NO = np.average(NO_ij, axis = -1, weights = weights)
     
@@ -66,8 +53,11 @@ def diag_fill(A, values):
     A[:, np.diag_indices(n)[0], np.diag_indices(n)[1]] = values
     return
 
-n_spe_max = 7 # maximal number of species
-n_com_prime = 1000 # number of communities at the beginning
+def geo_mean(A,axis = None):
+    return np.exp(np.nanmean(np.log(A), axis = axis))
+
+n_spe_max = 6 # maximal number of species
+n_com_prime = 10000 # number of communities at the beginning
 n_coms = np.zeros(n_spe_max+1, dtype = int)
 NO_all, FD_all  = np.full((2, n_spe_max+1, n_com_prime, n_spe_max), np.nan)
 A_all = np.full((n_spe_max+1, n_com_prime, n_spe_max, n_spe_max), np.nan)
@@ -75,34 +65,24 @@ equi_all = []
 equi_mono = []
 
 max_alpha = 0.3
-min_alpha = 0.1
+min_alpha = 0.01
 mu = 1
 n_specs = np.arange(2,n_spe_max + 1)
 
-diag_one = True
-symm = False
-title = "{}<a<{}".format(min_alpha, max_alpha)
-if diag_one:
-        title += "; a_ii = 1"
-if symm:
-        title += "; symm"
 # number of species ranging from 2 to 7
 for n in n_specs:
     # create random interaction matrices
-    #A_prime = np.exp(np.random.uniform(np.log(min_alpha),np.log(max_alpha)
-    #                ,size = (n_com_prime,n,n)))
+    A_prime = np.exp(np.random.uniform(np.log(min_alpha),np.log(max_alpha)
+                    ,size = (n_com_prime,n,n)))
     A_prime = np.random.uniform(min_alpha, max_alpha,size = (n_com_prime,n,n))
     
     # intraspecific competition is assumed to be 1
-    diag_fill(A_prime, np.random.uniform(1,2,(n_com_prime,n)))
-    if diag_one:
-        diag_fill(A_prime,1)
-    if symm:
-        A_prime = (A_prime + A_prime.swapaxes(1,2))/2
+    diag_fill(A_prime,1)
+    
     # intrinsic growth rate
     r_prime = np.ones((n_com_prime,n))
     
-    real, A, equi, sub_equi = find_real_communities(A_prime, r_prime)
+    real, A, sub_equi = find_real_communities(A_prime, r_prime)
     n_coms[n] = len(A)
     NO, FD = NFD_LV_multispecies(A,sub_equi)
     print(len(NO),n)
@@ -134,24 +114,32 @@ fs = 14
 
 # NO and FD versus species richness    
 fig = plt.figure(figsize = (11,11))
-ax_NO = fig.add_subplot(2,2,1)
-ax_NO.boxplot(ND_box, positions = n_specs,
+ax_ND = fig.add_subplot(2,2,1)
+ax_ND.boxplot(ND_box, positions = n_specs,
               showfliers = False)
-ax_NO.plot(n_specs, np.nanmean(ND_all[2:], axis = (1,2)), 'o')
-ax_NO.set_ylabel(r"$\mathcal{NO}$")
 
+ax_ND.plot(n_specs, geo_mean(ND_all[2:], axis = (1,2)), 'ro')
+ax_ND.plot(n_specs, np.nanmean(ND_all[2:], axis = (1,2)), 'ro')
+ax_ND.set_ylabel(r"$\mathcal{ND}$")
 
-ax_FD = fig.add_subplot(2,2,3, sharex = ax_NO)
+alpha_geom = geo_mean(A_all[2,:,0,1])
+ax_ND.set_xlim(1.5, n_spe_max + 0.5)
+
+ax_ND.axhline(np.nanmean(ND_box[0]), color = "red", label = "theory")
+ax_ND.legend()
+
+ax_FD = fig.add_subplot(2,2,3, sharex = ax_ND)
 ax_FD.boxplot(FD_box, positions = n_specs, showfliers = False)
 ax_FD.set_xlabel("number of species")
 ax_FD.set_ylabel(r"$\mathcal{F}$", fontsize = fs)
-ax_FD.plot(n_specs, np.nanmean(FD_all[2:], axis = (1,2)), 'o')
-ax_NO.set_xlim(1.5, n_spe_max + 0.5)
-# effect of ND and FD on relative yield
+ax_FD.plot(n_specs, 1-geo_mean(1-FD_all[2:], axis = (1,2)), 'ro')
+
+ax_FD.plot(n_specs, 1-(n_specs-1)/(1+alpha_geom*(n_specs-2)), color = "red")
+
 
 ax_coex = fig.add_subplot(1,2,2)
 x = np.linspace(0,1,1000)
-if False:
+if True:
     im = ax_coex.scatter(ND_all[2:, :, 0], FD_all[2:, :, 0], s = 16,
         c = n_specs.reshape(-1,1)*np.ones(NO_all[2:,:,0].shape),
         linewidth = 0, alpha = 0.5)
@@ -164,12 +152,41 @@ if False:
 
     cbar = fig.colorbar(im,ax = ax_coex)
     cbar.ax.set_ylabel("species richness")
+    fig.savefig("Figure, NFD effect on RYT,{},{}.png".format(
+        min_alpha, max_alpha))
+    
 
-alpha_geom = np.sqrt(min_alpha*max_alpha)
-alpha_av = (min_alpha + max_alpha)/2
-ax_FD.plot(n_specs, 1-(n_specs-1)/(1+alpha_av*(n_specs-2)))
-ax_FD.plot(n_specs, 1-(n_specs-1)/(1+alpha_geom*(n_specs-2)))
+###############################################################################
+# checkup the results
 
-fig.savefig("Figure, NFD effect on RYT,{},{},{},{}.png".format(
-        min_alpha, max_alpha, symm, diag_one))
-#"""
+# compute geometric mean interaction strength   
+NO_geom = geo_mean(NO_all[2:], axis = -1)
+FD_geom = 1 - geo_mean(1-FD_all[2:], axis = -1)
+geom_interact = geo_mean(A_all[2:], axis = (2,3))
+geom_interact = geom_interact**(n_specs/(n_specs-1)).reshape(-1,1)
+ND_diff_geom = NO_geom - geom_interact
+
+fig, ax = plt.subplots(2,1, figsize = (7,7), sharex = True)
+ax[0].boxplot(ND_diff_geom.T/(1-NO_geom.T), positions = range(2,n_spe_max +1))
+ax[0].set_ylabel(r"$(ND-(1-\bar{\alpha}))/ND$")
+FD_predict = (1 - (n_specs-1))/(1 + geom_interact.T*(n_specs-2))
+FD_diff = (FD_geom - FD_predict.T)/FD_geom
+ax[1].boxplot(FD_diff.T, positions = range(2,n_spe_max +1))
+
+ax[1].set_xlabel("species richness")
+ax[1].set_ylabel("(FD-FD_predict)/FD")
+fig.savefig("Expected ND_values, geom, simulated.pdf")
+
+# compute arithmetic mean interaction strengh
+NO_artm = np.nanmean(NO_all[2:], axis = -1)
+artm_interact = np.nanmean(A_all[2:], axis = (2,3))
+artm_interact = artm_interact*n_specs.reshape(-1,1)**2-n_specs.reshape(-1,1)
+artm_interact = artm_interact/(n_specs*(n_specs-1)).reshape(-1,1)
+
+
+ND_diff_artm = NO_artm - artm_interact
+fig = plt.figure()
+plt.boxplot(ND_diff_geom.T/(1-NO_geom.T))
+plt.xlabel("species richness")
+plt.ylabel(r"$(ND-(1-\bar{\alpha}))/ND$")
+fig.savefig("Expected ND_values, artm, simulated.pdf")
